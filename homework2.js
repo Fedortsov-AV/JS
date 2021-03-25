09d8e2b7cb7ce02778803ca8c90a40cc43e4c09b
Александр Федорцов <71198418+Fedortsov-AV@users.noreply.github.com>
2021-03-25 23:07:15 +1000

    Исправлена условие, когда  a и b положительны

--- homework2.js
+++ homework2.js
@@ -41,10 +41,9 @@ var x = 1 + (a *= 2);
 
 a = 5;
 b = -10;
-if (a > 0 && b > 0){
+if (a => 0 && b => 0){
   alert(a-b);
-}
-if (a < 0 && b < 0){
+else if (a < 0 && b < 0){
   alert(a*b);
 }
 if ((a < 0 && b >= 0) || (a >= 0 && b < 0)){
